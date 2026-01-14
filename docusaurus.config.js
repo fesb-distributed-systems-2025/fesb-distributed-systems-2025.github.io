b479cf7a6f751ef745a455c039333475067bca9f
Željko Kalajžić <zeljko.kalajzic>
2026-01-15 00:37:18 +0100

    lab10 update

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -10,7 +10,7 @@ const config = {
   tagline: '',
   url: 'https://fesb-distributed-systems-2025.github.io',
   baseUrl: '/',
-  onBrokenLinks: 'throw',
+  onBrokenLinks: 'warn',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
   organizationName: 'fesb-distributed-systems-2025',
